d20f1840568c072338769d98b3284c2833dbd206
Engr-M-Bilal-Khalid <muhammadbilal41266@gmail.com>
2026-05-31 20:40:38 +0500

    Update footer copyright name from M Bilal Khalid to Abdul Wajidv

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
           return (
                     <footer className="bg-gray-900 text-gray-400 text-center py-6 mt-12">
-                              <p>&copy; {new Date().getFullYear()} M Bilal Khalid. All rights reserved.</p>
+                              <p>&copy; {new Date().getFullYear()} Abdul Wajid. All rights reserved.</p>
                               <div className="space-x-4 mt-2">
                                         <a href="https://github.com" className="hover:text-white">GitHub</a>
                                         <a href="https://linkedin.com" className="hover:text-white">LinkedIn</a>
